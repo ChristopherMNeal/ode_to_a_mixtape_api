99a9515f14ffcdbd65b6ab365821b7e0bc22c47a
Christopher Neal <christopher.m.neal@gmail.com>
2025-06-02 13:23:28 -0500

    fix normalizable issue

---
+++ db/migrate/20250601235828_remove_unique_constraint_from_broadcasts_normalized_title.rb
@@ -0,0 +1,6 @@
+class RemoveUniqueConstraintFromBroadcastsNormalizedTitle < ActiveRecord::Migration[7.1]
+  def change
+    remove_index :broadcasts, :normalized_title
+    add_index :broadcasts, :normalized_title, unique: false
+  end
+end
